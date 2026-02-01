676ea4a736f38570ea2f724bc6b889293db17dbb
Filipe Paula (Fsp30) <depaulacostafilipe@gmail.com>
2026-02-01 19:50:17 -0300

    remove unneccessary methods in class TeamId

--- src/domain/value-objects/TeamId.ts
+++ src/domain/value-objects/TeamId.ts
@@ -1,29 +1,12 @@
 import { randomUUID } from 'crypto';
+import { Id } from './Id';
 
-export class TeamId {
-  private constructor(public readonly value: string) {
-    this.validate();
-  }
-
+export class TeamId extends Id {
   static create(value?: string): TeamId {
     return new TeamId(value ?? randomUUID());
   }
 
   static fromString(value: string): TeamId {
     return new TeamId(value);
-  }
-
-  private validate(): void {
-    if (!this.value || this.value.trim().length === 0) {
-      throw new Error('TeamId não pode ser vazio');
-    }
-  }
-
-  equals(other: TeamId): boolean {
-    return this.value === other.value;
-  }
-
-  toString(): string {
-    return this.value;
   }
 }

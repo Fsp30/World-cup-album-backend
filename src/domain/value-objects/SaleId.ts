0145a28db0a621949acb47cd57cc38e0b27f5f69
Filipe Paula (Fsp30) <depaulacostafilipe@gmail.com>
2026-02-01 19:46:32 -0300

    remove unneccessary methods in class SaleId

--- src/domain/value-objects/SaleId.ts
+++ src/domain/value-objects/SaleId.ts
@@ -1,29 +1,12 @@
 import { randomUUID } from 'crypto';
+import { Id } from './Id';
 
-export class SaleId {
-  private constructor(public readonly value: string) {
-    this.validate();
-  }
-
+export class SaleId extends Id {
   static create(value?: string): SaleId {
     return new SaleId(value ?? randomUUID());
   }
 
   static fromString(value: string): SaleId {
     return new SaleId(value);
-  }
-
-  private validate(): void {
-    if (!this.value || this.value.trim().length === 0) {
-      throw new Error('SaleId não pode ser vazio');
-    }
-  }
-
-  equals(other: SaleId): boolean {
-    return this.value === other.value;
-  }
-
-  toString(): string {
-    return this.value;
   }
 }

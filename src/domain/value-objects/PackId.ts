95426a8e167d5c36f937f29b8520651f8dba827c
pedrohz1 <178624047+pedrohz1@users.noreply.github.com>
2026-02-03 00:19:15 -0300

    Edit PackId.ts to extend Id

--- src/domain/value-objects/PackId.ts
+++ src/domain/value-objects/PackId.ts
@@ -1,29 +1,12 @@
 import { randomUUID } from 'crypto';
+import { Id } from './Id';
 
-export class PackId {
-  private constructor(public readonly value: string) {
-    this.validate();
-  }
-
+export class PackId extends Id {
   static create(value?: string): PackId {
     return new PackId(value ?? randomUUID());
   }
 
   static fromString(value: string): PackId {
     return new PackId(value);
-  }
-
-  private validate(): void {
-    if (!this.value || this.value.trim().length === 0) {
-      throw new Error('PackId não pode ser vazio!');
-    }
-  }
-
-  equals(other: PackId): boolean {
-    return this.value === other.value;
-  }
-
-  toString(): string {
-    return this.value;
   }
 }

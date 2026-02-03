0006863999de20c2a15bd56bf67702ba06cc756d
pedrohz1 <178624047+pedrohz1@users.noreply.github.com>
2026-02-03 00:31:57 -0300

    Add User and Pack to index and tests

--- src/__tests__/domain/value-objects/RunId.test.ts
+++ src/__tests__/domain/value-objects/RunId.test.ts
@@ -2,17 +2,19 @@ import {
   AlbumId,
   CardId,
   MissionId,
+  PackId,
   TeamId,
   TradeId,
+  UserId
 } from '@/domain/value-objects';
 import { runIdBaseTests } from '../../../utils/tests/IdBaseUtils';
 import { SaleId } from '@/domain/value-objects/SaleId';
 import { DuelId } from '@/domain/value-objects/DuelId';
 
-//runIdBaseTests('UserId', UserId);
+runIdBaseTests('UserId', UserId);
 runIdBaseTests('CardId', CardId);
 runIdBaseTests('AlbumId', AlbumId);
-//runIdBaseTests('PackId', PackId);
+runIdBaseTests('PackId', PackId);
 runIdBaseTests('TradeId', TradeId);
 runIdBaseTests('SaleId', SaleId);
 runIdBaseTests('DuelId', DuelId);
--- src/domain/value-objects/index.ts
+++ src/domain/value-objects/index.ts
@@ -1,10 +1,10 @@
 export * from './business';
 export * from './Id';
 
-//export * from './UserId';
+export * from './UserId';
 export * from './CardId';
 export * from './AlbumId';
-//export * from './PackId';
+export * from './PackId';
 export * from './TradeId';
 export * from './SaleId';
 export * from './TeamId';

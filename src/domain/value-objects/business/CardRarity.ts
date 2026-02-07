fd5c833fe02c824254c5e0947ee23c7f53590645
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-02-07 01:18:13 -0300

    Create SALE_VALUES

--- src/domain/value-objects/business/CardRarity.ts
+++ src/domain/value-objects/business/CardRarity.ts
@@ -8,11 +8,19 @@ export enum RarityLevel {
 
 export class CardRarity {
     private static readonly DROP_RATES: Record<RarityLevel, number> = {
-        [RarityLevel.COMMON]:
-        [RarityLevel.UNCOMMON]:
-        [RarityLevel.RARE]:
-        [RarityLevel.EPIC]:
-        [RarityLevel.LEGENDARY]:
+        [RarityLevel.COMMON]: 1,
+        [RarityLevel.UNCOMMON]: 2,
+        [RarityLevel.RARE]: 3,
+        [RarityLevel.EPIC]: 4,
+        [RarityLevel.LEGENDARY]: 5,
+    }
+
+    private static readonly SALE_VALUES: Record<RarityLevel, number> = {
+        [RarityLevel.COMMON]: 1,
+        [RarityLevel.UNCOMMON]: 2,
+        [RarityLevel.RARE]: 3,
+        [RarityLevel.EPIC]: 4,
+        [RarityLevel.LEGENDARY]: 5,
     }
 
 }

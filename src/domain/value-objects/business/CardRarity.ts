d0475d712f513b11aa84f2aece8e16fd7a4127ff
Ricardo06rib <ricardo.fernandes.ribeiro.06@gmail.com>
2026-02-07 17:55:31 -0300

    create random method of CardRarity class

--- src/domain/value-objects/business/CardRarity.ts
+++ src/domain/value-objects/business/CardRarity.ts
@@ -8,11 +8,11 @@ export enum RarityLevel {
 
 export class CardRarity {
     private static readonly DROP_RATES: Record<RarityLevel, number> = {
-        [RarityLevel.COMMON]: 1,
-        [RarityLevel.UNCOMMON]: 2,
-        [RarityLevel.RARE]: 3,
-        [RarityLevel.EPIC]: 4,
-        [RarityLevel.LEGENDARY]: 5,
+        [RarityLevel.COMMON]: 0, // 50%
+        [RarityLevel.UNCOMMON]: 0.5, // 30%
+        [RarityLevel.RARE]: 0.8, // 15%
+        [RarityLevel.EPIC]: 0.95, // 4,5%
+        [RarityLevel.LEGENDARY]: 0.995 //0,5%
     }
 
     private static readonly SALE_VALUES: Record<RarityLevel, number> = {
@@ -29,6 +29,17 @@ export class CardRarity {
 
     static create(level: RarityLevel): CardRarity {
         return new CardRarity(level);
+    }
+
+    static random(): CardRarity{
+        const drop: number = Math.random();
+        
+        if(CardRarity.DROP_RATES[RarityLevel.COMMON] <= drop && drop < CardRarity.DROP_RATES[RarityLevel.UNCOMMON])     return new CardRarity(RarityLevel.COMMON);
+        if(CardRarity.DROP_RATES[RarityLevel.UNCOMMON] <= drop && drop < CardRarity.DROP_RATES[RarityLevel.UNCOMMON])   return new CardRarity(RarityLevel.UNCOMMON);
+        if(CardRarity.DROP_RATES[RarityLevel.RARE] <= drop && drop < CardRarity.DROP_RATES[RarityLevel.UNCOMMON])       return new CardRarity(RarityLevel.RARE);
+        if(CardRarity.DROP_RATES[RarityLevel.EPIC] <= drop && drop < CardRarity.DROP_RATES[RarityLevel.UNCOMMON])       return new CardRarity(RarityLevel.EPIC);
+        if(CardRarity.DROP_RATES[RarityLevel.LEGENDARY] <= drop && drop < 1)                                            return new CardRarity(RarityLevel.LEGENDARY);
+        throw new Error(`Número gerado ${drop} não se encaixa nas faixas de drop`);
     }
 
     static fromString(level:string): CardRarity {
